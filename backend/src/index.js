c04be823dc95c6d28f85c9780dc10b8c1171d79c
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-20 12:59:37 -0300

    Add, perfumaria

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ messsage: 'Hello World'});
+  return response.json({ messsage: 'Hello World 2'});
 });
 
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 back-end started!');
+});
