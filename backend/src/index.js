4309db1a73e9b50d5bb28a3bd73451f26ea71970
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-21 09:52:36 -0300

    Add, parametros, rotas, backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,16 @@ const { response } = require('express');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+
+
 
 app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  console.log(title);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -12,6 +20,9 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -20,6 +31,9 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
