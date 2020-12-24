8e88fa62ef7582ceb41383c148de683c7dc3bcbb
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-24 18:59:30 -0300

    Add, estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`);
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Link 1</li>
-          <li>Link 2</li>
-        </ul>
-      </Header>  
+      <Header title="Homepage" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>  
+
+      <button type="button" onClick={handleAddProject}>Adcionar projeto</button>
     </>
   )
 }
