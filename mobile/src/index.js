0ca1f8e607b31678edf56fe3d119f5cff5889c1e
eduardonk9999 <thiago.novak7@gmail.com>
2020-12-30 14:21:01 -0300

    Add, conceitos iniciais react native

---
+++ mobile/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+export default function App() {
+    return (
+        <>
+            <StatusBar  barStyle="light-content" />
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello Edu</Text>
+            </View> 
+        </>   
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
